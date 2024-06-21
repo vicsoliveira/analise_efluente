2f3c7225820cea200f7f44120f46571d9600a948
vicsoliveira <116915472+vicsoliveira@users.noreply.github.com>
2024-06-21 12:19:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,31 +7,32 @@ def read_and_organize_data(file):
     st.write("Primeiras linhas do DataFrame após leitura:")
     st.write(df.head(20))
 
-    # Identificar as seções de dados
-    coleta_indices = df[df.iloc[:, 1].str.contains('Coleta', na=False)].index
+    # Procurar todas as linhas onde os dados realmente começam
+    start_rows = df[df.iloc[:, 1].str.contains('Coleta', na=False)].index.tolist()
 
     data_list = []
-    for index in coleta_indices:
-        coleta_data = df.iloc[index:index+10, :].reset_index(drop=True)
-        coleta = coleta_data.iloc[1, 2]
-        elaboracao = coleta_data.iloc[2, 2]
-        amostra = coleta_data.iloc[4, 2]
-        
-        parametros_data = df.iloc[index+10:index+20, 1:7].reset_index(drop=True)
-        parametros_data.columns = ["Parâmetro", "Valor obtido", "Unidade", "Valor mínimo", "Valor máximo", "Resultado"]
+    for start_row in start_rows:
+        coleta = df.iloc[start_row + 1, 2]
+        elaboracao = df.iloc[start_row + 2, 2]
+        amostra = df.iloc[start_row + 4, 2]
 
+        # Coletar todos os parâmetros analisados
+        parametros_data = df.iloc[start_row + 6:start_row + 20].reset_index(drop=True)
+        parametros_data.columns = ["Parâmetro", "Valor obtido", "Unidade", "Valor mínimo", "Valor máximo", "Resultado"]
+        
         for _, row in parametros_data.iterrows():
-            data_list.append({
-                "Coleta": coleta,
-                "Elaboração do Laudo": elaboracao,
-                "Amostra": amostra,
-                "Parâmetro": row["Parâmetro"],
-                "Valor obtido": row["Valor obtido"],
-                "Unidade": row["Unidade"],
-                "Valor mínimo": row["Valor mínimo"],
-                "Valor máximo": row["Valor máximo"],
-                "Resultado": row["Resultado"]
-            })
+            if pd.notna(row["Parâmetro"]):
+                data_list.append({
+                    "Coleta": coleta,
+                    "Elaboração do Laudo": elaboracao,
+                    "Amostra": amostra,
+                    "Parâmetro": row["Parâmetro"],
+                    "Valor obtido": row["Valor obtido"],
+                    "Unidade": row["Unidade"],
+                    "Valor mínimo": row["Valor mínimo"],
+                    "Valor máximo": row["Valor máximo"],
+                    "Resultado": row["Resultado"]
+                })
     
     df_final = pd.DataFrame(data_list)
     
